ae4f23f827706825cd5e04bd627514219dcc819c
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2021-01-10 15:03:49 -0400

    primeras rutas y Nodemon

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
 const app = express();
+
+app.get('/api/athletes', (req, res) => {
+    res.send('hola desde api atletas')
+});
+
+
+app.get('/api/sports', (req, res) => {
+    res.send('hola desde api sports')
+});
 
 app.listen(8080, () => {
     console.log('app listening on port 8080')
